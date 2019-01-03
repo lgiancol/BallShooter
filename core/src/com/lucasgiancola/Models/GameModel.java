7755fc3bce331a4cd20cd69ea881d41f48ffbad7
Lucas Giancola <lgiancol@uoguelph.ca>
2019-01-03 17:13:43 -0500

    The angle of the ball is calculated from wherever the user is currently touching the screen

--- core/src/com/lucasgiancola/Models/GameModel.java
+++ core/src/com/lucasgiancola/Models/GameModel.java
@@ -1,7 +1,16 @@
 package com.lucasgiancola.Models;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.lucasgiancola.BallShooter;
+import com.lucasgiancola.Objects.Ball;
+
 public class GameModel {
     private boolean gameInProgress = false;
+
+    private Vector2 pivot;
+    private Vector2 touch = null;
 
     // When to create a new ball
     private float newBallCounter = 0f;
@@ -11,14 +20,45 @@ public class GameModel {
     private float newRowCounter = 0f;
     private float deltaTimeNewRow = 1f;
 
+    private Stage stage = null;
+
     public GameModel() {
         this.gameInProgress = true;
+
+        pivot = new Vector2(BallShooter.WIDTH / 2, 0);
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
 
     // Update anything that has to do with the model of the game
     public void update(float delta) {
         newBallCounter += delta;
         newRowCounter += delta;
+
+        if(Gdx.input.isTouched()) {
+            this.updateTouchPositions();
+        }
+    }
+
+    // Will set the touch position based on the stage
+    private void updateTouchPositions() {
+        Vector2 temp = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+        if(this.stage != null) {
+            touch = stage.getViewport().unproject(temp);
+        }
+    }
+
+    public float getTouchAngle() {
+        if(touch != null) {
+            Vector2 temp = new Vector2(touch.x, touch.y);
+            temp.sub(pivot);
+            temp.nor();
+            return temp.angle();
+        }
+
+        return 90;
     }
 
     public boolean isPlaying() { return gameInProgress; }
---
+++ core/src/com/lucasgiancola/Objects/AngleHelper.java
@@ -0,0 +1,43 @@
+package com.lucasgiancola.Objects;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class AngleHelper extends Actor {
+    private ShapeRenderer sr;
+    private Vector2 start;
+    private Vector2 end;
+
+    private float angle = 90;
+
+    public AngleHelper(Vector2 start) {
+        this.sr = new ShapeRenderer();
+
+        this.start = start;
+    }
+
+    public void draw(Batch batch, float parentAngle) {
+        if(end == null) return;
+
+        batch.end();
+        sr.begin(ShapeRenderer.ShapeType.Line);
+        sr.setProjectionMatrix(batch.getProjectionMatrix());
+
+        sr.line(start, end);
+
+        sr.end();
+        batch.begin();
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+
+        float fx = (float) (3000 * Math.cos(this.angle * MathUtils.degreesToRadians));
+        float fy = (float) (3000 * Math.sin(this.angle * MathUtils.degreesToRadians));
+
+        this.end = new Vector2(this.start.x, this.start.y).add(fx, fy);
+    }
+}
